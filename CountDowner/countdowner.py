0861b70be64c713bd6227b61da06111a93f8fffa
Trupera <66578152+Trupera@users.noreply.github.com>
2025-11-07 23:56:48 -0500

    basic countdown code

--- CountDowner/countdowner.py
+++ CountDowner/countdowner.py
@@ -1,22 +1,51 @@
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import QTimer, QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QGridLayout, QStackedLayout, QWidget
 import time
 import datetime
 import sys
 
 class CreatorWindow(QWidget): #This will be the window where users can create their timers
-     def __init__(self):
+    def __init__(self):
         super().__init__()
 
         self.setWindowTitle("CountDowner Creator")
         self.layout = QStackedLayout()
-        self.label = QLabel("Another Window")
-        self.layout.addWidget(self.label)
+
+        self.timer_label = QLabel("00:00:00")
+        font = self.timer_label.font()
+        font.setPointSize(30)
+        self.timer_label.setFont(font)
+        self.timer_label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+
+        self.layout.addWidget(self.timer_label)
         self.setLayout(self.layout)
 
         #Creator Window Limits
         self.setMinimumSize(QSize(700,600))
         self.setMaximumSize(QSize(1920,1080))
+
+        #timer
+        self.duration_seconds = 10
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_countdown)
+        self.timer.start(1000)
+    def update_countdown(self):
+        if self.duration_seconds > 0:
+            self.duration_seconds -= 1
+
+            hours = self.duration_seconds // 3600
+            minutes = (self.duration_seconds % 3600) // 60
+            seconds = self.duration_seconds % 60
+
+            time_str = f"{hours:02}:{minutes:02}:{seconds:02}"
+            self.timer_label.setText(time_str)
+        else:
+            # 5. Stop the timer when it reaches zero
+            self.timer.stop()
+            self.timer_label.setText("Done!")
+
+        
 
 class MainWindow(QMainWindow):  #QMainWindow is the parent class
     def __init__(self):
